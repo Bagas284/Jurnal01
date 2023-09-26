b46abd0acf9c3236970b94f96105346634f2ac88
Bagas284 <145474170+Bagas284@users.noreply.github.com>
2023-09-26 13:20:49 +0700

    first commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,7 +1,7 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        System.out.println("Nama : Bagas Aldianata");
-        System.out.println("NIM : 607062300060");
-        System.out.println("Kelas : D3IF-47-03");
+        System.out.println("Nama: Bagas Aldianata");
+        System.out.println("NIM: 607062300060");
+        System.out.println("Kelas: D3IF-47-03");
     }
 }
